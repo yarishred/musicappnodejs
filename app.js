f1fb8a4326b7436f5019b409c74d59c1c9e30067
yarishred <yfruiz9@gmail.com>
2021-10-11 11:57:54 -0500

    First Commit for the music app, working on the player

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 const port = 5500;
 
 // Store Mongodb Session
-const MongoDBURI = "mongodb+srv://yarishred:Yari15447419@cluster0.vhvmi.mongodb.net/musicdb?retryWrites=true&w=majority";
+const MongoDBURI = "mongodb+srv://xxxxxxxxxxxx@cluster0.vhvmi.mongodb.net/musicdb?retryWrites=true&w=majority";
 ;
 const store = new MongoDBStore({
   uri: MongoDBURI,
